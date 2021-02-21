a6324650c78fe97c6824d380bb0ccd5bb5c98ed5
BaoanhTrinh <anhvancham28052000@gmail.com>
2021-02-21 21:09:04 +0700

    my first message

---
+++ QuanLyThuVienHVKTQS/db/dbo.docgia.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[docgia] (
+    [sothe]      INT           NOT NULL,
+    [hoten]      VARCHAR (50)  NULL,
+    [email]      VARCHAR (50)  NULL,
+    [ngaysinh]   DATETIME      NULL,
+    [diachi]     VARCHAR (50)  NULL,
+    [socmtnd]    VARCHAR (50)  NULL,
+    [anhthe]     VARCHAR (50)  NULL,
+    [ngaylamthe] DATETIME      NULL,
+    [handungthe] DATETIME      NULL,
+    [note]       VARCHAR (250) NULL, 
+    CONSTRAINT [PK_docgia] PRIMARY KEY ([sothe]) 
+);
+
---
+++ QuanLyThuVienHVKTQS/db/dbo.muonsach.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[muonsach] (
+    [id]       INT           NULL,
+    [masach]   INT           NULL,
+    [tennv]    VARCHAR (250) NULL,
+    [sothe]    INT           NULL,
+    [ngaymuon] DATETIME      NULL,
+    [hantra]   DATETIME      NULL,
+    [ngaytra]  DATETIME      NULL
+);
+
